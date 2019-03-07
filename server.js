d75cdd303611151d93c37505e14c7697f6d9c3d9
liyanghsiao <liyanghsiao@boyu66.net>
2019-03-07 08:57:25 +0800

    添加測試用的server.js

---
+++ server.js
@@ -0,0 +1,423 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const getBody = (odds, cron_status) => ({
+    "status": 200,
+    "data": [
+      {
+        "_id": "5c7f6738cdf929752ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "grg",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7a67df9sss29752ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "taaaest2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sss29752ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "taaaest2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67dd9sss29752ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "tesdfasdst2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sss12352ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "teasdasst2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sss29752eda2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "tessdasdt2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sds29752ede2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "tevcdvdfsst2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sss2975123a2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "teasd12st2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67df9sss2dsaw123a2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "tesasdsadt2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f67dasd1ss2975123a2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "tes12312t2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7f6asd12ss2975123a2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "testasdasd2",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      },
+      {
+        "_id": "5c7fasdf9sss2975123a2d78",
+        "fancyData": [
+          {
+            "cron_status": cron_status,
+            "NoValume": 100,
+            "YesValume": 100,
+            "SessInptYes": odds,
+            "SessInptNo": 1,
+            "DisplayMsg": ""
+          }
+        ],
+        "split": false,
+        "status": false,
+        "timeSet": null,
+        "inPlay": false,
+        "updateTimeStatus": false,
+        "inPlayTime": 3,
+        "match_id": 29163335,
+        "title": "New Zealand v Bangladesh (2nd Test)",
+        "match_market_id": "1.155693649",
+        "headname": "test2asdsa",
+        "max_bet": 5000,
+        "min_bet": 500,
+        "cron_status": cron_status,
+        "DisplayMsg": "",
+        "maxLiabilityPerBet": 1000,
+        "max_profit": 10000,
+        "bet_delay": 0,
+        "result": null,
+        "createdAt": "2019-03-06T11:52:48.089Z",
+        "updatedAt": "2019-03-06T11:52:48.089Z",
+        "__v": 0
+      }
+    ]
+  })
+let isBallRunning = false;
+app.use(async ctx => {
+    isBallRunning = !isBallRunning;
+    ctx.body = getBody(Math.floor(Date.now()/100 % 1000), isBallRunning ? 0 : 1);
+  });
+  
+app.listen(3000);
