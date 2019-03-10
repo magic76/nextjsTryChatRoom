93245872b8ad649db3382d9df9820165fd6fa1f9
liyanghsiao <liyanghsiao@boyu66.net>
2019-03-10 15:15:30 +0800

    upadt

--- server.js
+++ server.js
@@ -143,12 +143,12 @@ let data = [
     "_id": "5c7f67df9sss29fdsefdsew78",
     "fancyData": [
       {
-        "cron_status": 1,
+        "cron_status": 0,
         "NoValume": 90,
         "YesValume": 90,
         "SessInptYes": 2,
         "SessInptNo": 3,
-        "DisplayMsg": ""
+        "DisplayMsg": "SUSPEND"
       }
     ],
     "split": false,
