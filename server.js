4cfb45f8abb05935037c561832a289707b57e58b
liyanghsiao <liyanghsiao@boyu66.net>
2019-03-10 14:40:58 +0800

    tupdas

--- server.js
+++ server.js
@@ -138,6 +138,41 @@ let data = [
     "updatedAt": 1,
     "__v": 0
   },
+
+  {
+    "_id": "5c7f67df9sss29fdsefdsew78",
+    "fancyData": [
+      {
+        "cron_status": 1,
+        "NoValume": 100,
+        "YesValume": 100,
+        "SessInptYes": 1,
+        "SessInptNo": 1,
+        "DisplayMsg": ""
+      }
+    ],
+    "split": false,
+    "status": false,
+    "timeSet": null,
+    "inPlay": false,
+    "updateTimeStatus": false,
+    "inPlayTime": 3,
+    "match_id": 29163335,
+    "title": "New Zealand v Bangladesh (2nd Test)",
+    "match_market_id": "1.155693649",
+    "headname": "TEST999",
+    "max_bet": 5000,
+    "min_bet": 500,
+    "cron_status": 1,
+    "DisplayMsg": "",
+    "maxLiabilityPerBet": 1000,
+    "max_profit": 10000,
+    "bet_delay": 0,
+    "result": null,
+    "createdAt": "2019-03-06T11:52:48.089Z",
+    "updatedAt": 1,
+    "__v": 0
+  },
 ];
 const getBody = (odds, cron_status) => {
   data[2].fancyData[0].cron_status = cron_status;
